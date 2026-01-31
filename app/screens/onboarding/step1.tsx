37eb074f30361c53b2193fa100d0120aeae2056e
Wizuha <williams.wandji@gmail.com>
2026-01-31 21:18:31 +0100

    update layout

--- app/screens/onboarding/step1.tsx
+++ app/screens/onboarding/step1.tsx
@@ -1,20 +1,12 @@
-import { Stack } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Step1() {
   return (
     <>
-      <Stack.Screen options={{ headerShown: false }} />
       <View>
-        <Text style={styles.container}>Step 1</Text>
+        <Text>Step 1</Text>
       </View>
     </>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    fontSize: 40,
-    color: "#FFF",
-  },
-});
