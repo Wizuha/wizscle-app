65e78d02b4e2575bd7ed37e8b2503bb08e8684d5
Wizuha <williams.wandji@gmail.com>
2026-01-31 20:47:25 +0100

    deleted header auto step1

--- app/screens/onboarding/step1.tsx
+++ app/screens/onboarding/step1.tsx
@@ -1,10 +1,20 @@
+import { Stack } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Step1() {
   return (
-    <View>
-      <Text>Step 1</Text>
-    </View>
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View>
+        <Text style={styles.container}>Step 1</Text>
+      </View>
+    </>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    fontSize: 40,
+    color: "#FFF",
+  },
+});
