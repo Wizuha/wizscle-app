888f1d540a7daa5bb4e3807545065c376cf387d4
Wizuha <williams.wandji@gmail.com>
2026-02-04 23:19:07 +0100

    started step 1 screen

--- app/screens/onboarding/step1.tsx
+++ app/screens/onboarding/step1.tsx
@@ -1,12 +1,41 @@
+import { COLORS } from "@/app/constants/color";
+import { Text } from "@react-navigation/elements";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function Step1() {
   return (
-    <>
-      <View>
-        <Text>Step 1</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Disponibilité</Text>
+        <Text style={styles.subtitle}>Planifons vos entraînements</Text>
       </View>
-    </>
+      <View style={styles.content}></View>
+      <View style={styles.footer}></View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+  header: {
+    flex: 1,
+    marginTop: 90,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    paddingHorizontal: 32,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 20,
+    color: "#9CA3AF",
+  },
+  content: {},
+  footer: {},
+});
